9f6bb06cb78a9b74201988f46f09610108bae957
zhu <496783284@qq.com>
2021-02-23 10:09:51 +0800

    chore: 完善目录结构

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@ import App from './App'
 import router from './router'
 import store from './store'
 
+// 样式
 import '@/styles/index.scss'
+// 图标
 import '@/icons/index.css'
+// 路由权限
 import '@/permission'
+// 小部件
 import '@/widgets/element-ui'
 
 new Vue({
---
+++ src/widgets/directives.js
@@ -0,0 +1,3 @@
+import Vue from 'vue'
+// import xxx from '@/directives'
+// Vue.directive(xxx, xxx)
