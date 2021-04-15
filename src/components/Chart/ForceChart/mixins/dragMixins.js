efedfb3164095feb820e1e3ae912e419c6301310
zhu <496783284@qq.com>
2021-04-15 09:52:43 +0800

    feat: drag mixins

--- src/components/Chart/ForceChart/mixins/dragMixins.js
+++ src/components/Chart/ForceChart/mixins/dragMixins.js
@@ -4,33 +4,31 @@ export default {
   data() {
     return {
       dragHandler: null,
+      dragging: false,
     }
   },
   methods: {
     initDragHandler() {
       const { forceSimulation } = this
-      function dragstarted(event) {
-        if (!event.active) forceSimulation.alphaTarget(0.3).restart()
-        event.subject.fx = event.subject.x
-        event.subject.fy = event.subject.y
-      }
-
-      function dragged(event) {
-        event.subject.fx = event.x
-        event.subject.fy = event.y
-      }
-
-      function dragended(event) {
-        if (!event.active) forceSimulation.alphaTarget(0)
-        event.subject.fx = null
-        event.subject.fy = null
-      }
 
       this.dragHandler = d3
         .drag()
-        .on('start', dragstarted)
-        .on('drag', dragged)
-        .on('end', dragended)
+        .on('start', (event) => {
+          if (!event.active) forceSimulation.alphaTarget(0.3).restart()
+          event.subject.fx = event.subject.x
+          event.subject.fy = event.subject.y
+          this.dragging = true
+        })
+        .on('drag', (event) => {
+          event.subject.fx = event.x
+          event.subject.fy = event.y
+        })
+        .on('end', (event) => {
+          if (!event.active) forceSimulation.alphaTarget(0)
+          event.subject.fx = null
+          event.subject.fy = null
+          this.dragging = false
+        })
     },
   },
 }
